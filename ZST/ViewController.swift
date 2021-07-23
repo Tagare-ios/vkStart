3f65bbf092d188efa0a24545aee2587c6b28c8c1
Seva <Lulakov>
2021-07-23 15:57:22 +0300

    1lesson
    не смог понять как настроить скрольвю

---
+++ ZST/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ZST
+//
+//  Created by Сева on 15.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var input_name: UITextField!
+    @IBOutlet weak var Login_succseed_btn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
